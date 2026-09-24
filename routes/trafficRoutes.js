9eb3f5c21d9eb8bc4fcfc09085eb68c3e58a4c28
ameen373 <81308096+ameen373@users.noreply.github.com>
2026-09-24 17:33:38 +0300

    Add Arabic comments to trafficRoutes.js

--- routes/trafficRoutes.js
+++ routes/trafficRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const { validateTraffic, clickLimiter } = require('../middleware/rateLimiters');
 const { handleInitClick, handleImpression } = require('../controllers/trafficController');
 
+// مسارات بدء النقر
 router.post('/init-click', validateTraffic, handleInitClick);
 router.post('/api/init-click', validateTraffic, handleInitClick);
 
+// مسارات تسجيل الظهور
 router.post('/impression', validateTraffic, clickLimiter, handleImpression);
 router.post('/api/impression', validateTraffic, clickLimiter, handleImpression);
 
