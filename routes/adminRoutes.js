6a8672d958724930a63f2b6d94757072d44c0503
ameen373 <81308096+ameen373@users.noreply.github.com>
2026-09-25 16:59:00 +0300

    Delete routes directory

--- routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { adminMiddleware } = require('../middleware/auth');
-const { getAdminDashboardData } = require('../controllers/adminController');
-
-router.get('/admin/dashboard-data', adminMiddleware, getAdminDashboardData);
-router.get('/api/admin/dashboard-data', adminMiddleware, getAdminDashboardData);
-
-module.exports = router;
